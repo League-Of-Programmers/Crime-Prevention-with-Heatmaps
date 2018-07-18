46290a9b66b0ffea192ad4b1d09db71bbb8d419b
Anis Sarker <shahidsarker95@hotmail.com>
2018-07-18 02:17:38 +0000

    mongod/app.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var port = 3000;
-var host = '127.0.0.1';
+//var port = process.env.PORT;
+//var host = '127.0.0.1';
+
+require('dotenv').load();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
@@ -12,6 +15,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 require('./routes/core.routes.js')(app);
 
-app.listen(port, host, function(){
-    console.log(`Listening to http://${host}:${port}`);
+// app.listen(port, function(){
+//     //console.log(`Listening to http://${host}:${port}`);
+// });
+app.listen(port, process.env.IP, function(){
+   console.log("Server Has Started!");
 });
